09fb7b1694046e211f96a44406e39e93cc2cc644
Kazuya Iriguchi <kazuya.iriguchi.j6k@jp.denso.com>
2023-05-16 13:45:29 +0900

    add: convert method list to pose

--- scripts/robot_controller.py
+++ scripts/robot_controller.py
@@ -1,7 +1,7 @@
 import sys
 import rospy
-from moveit_commander import MoveGroupCommander, PlanningSceneInterface, roscpp_initialize, roscpp_shutdown
-from moveit_msgs.msg import RobotTrajectory
+import tf
+from moveit_commander import MoveGroupCommander, roscpp_initialize, roscpp_shutdown
 from geometry_msgs.msg import Pose
 
 class RobotController:
@@ -26,11 +26,27 @@ class RobotController:
         self.group.set_max_velocity_scaling_factor(scaling_factor)
         self.group.set_max_acceleration_scaling_factor(scaling_factor)
 
+    def list_to_pose(self, pose_list):
+        # pose_list is a list in the format [x, y, z, rx, ry, rz]
+        pose = Pose()
+        pose.position.x = pose_list[0]
+        pose.position.y = pose_list[1]
+        pose.position.z = pose_list[2]
+        quaternion = tf.transformations.quaternion_from_euler(pose_list[3], pose_list[4], pose_list[5])
+        pose.orientation.x = quaternion[0]
+        pose.orientation.y = quaternion[1]
+        pose.orientation.z = quaternion[2]
+        pose.orientation.w = quaternion[3]
+        return pose
+
+    def move_to_pose_list(self, pose_list):
+        pose = self.list_to_pose(pose_list)
+        self.move_to_pose(pose)
+
 if __name__ == "__main__":
     robot = RobotController("arm_group")
-    # pose = Pose()
-    # set your pose values
-    # robot.move_to_pose(pose)
+    pose_list = [0.132, -0.150, 0.075, 3.140, 0.027, 3.089]  # Example list
+    robot.move_to_pose_list(pose_list)
     print(robot.get_current_pose())
-    # robot.set_speed_scaling(0.5)
+    robot.set_speed_scaling(0.5)
 
